2b44b7f2e2df2c28d2e54fe77b63fbd5bca939dd
ZhentingYan <632950864@qq.com>
2019-07-06 09:35:22 +0800

    Upload the initial Architecture of the program.

---
+++ Adventure/Controllers/HomeStayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adventure.Controllers
+{
+    public class HomeStayController : Controller
+    {
+        // GET: HomeStay
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
