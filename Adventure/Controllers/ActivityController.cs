610c13405c7fdce935a38aa9faf207137167ae0d
Hyuhan <1753255@tongji.edu.cn>
2019-07-08 16:38:48 +0800

    ActivityPages

--- Adventure/Controllers/ActivityController.cs
+++ Adventure/Controllers/ActivityController.cs
@@ -13,5 +13,27 @@ namespace Adventure.Controllers
         {
             return View();
         }
+
+        // GET: Single Activity
+        public ActionResult SingleActivity()
+        {
+            ViewBag.Message = "The activity for more information page.";
+
+            return View();
+        }
+
+        // GET: Publish Activity
+        public ActionResult Publish()
+        {
+            if (Session["user_id"] == null)
+            {
+                return Redirect("~/Login/index");
+            }
+            else
+            {
+                ViewBag.Message = "Publish your own activity page.";
+                return View();
+            }
+        }
     }
 }
