8fc196b2c302137c64e0c8cdc94f526b862ede5a
ZhentingYan <632950864@qq.com>
2019-07-11 08:46:44 +0800

    搜索错误版本

--- Adventure/Controllers/ActivityController.cs
+++ Adventure/Controllers/ActivityController.cs
@@ -120,20 +120,15 @@ namespace Adventure.Controllers
 
 
         // GET: Activity
-        [HttpGet]
-        public ActionResult Index()
-        {
 
-            return View();
-        }
         [HttpPost]
         public ActionResult Index(FormCollection form)
         {
             SqlSugarClient db = new SqlSugarClient(
-                            new ConnectionConfig()
-                            {
-                                ConnectionString = System.Web.Configuration.WebConfigurationManager.AppSettings["ConnectionString"],
-                                DbType = DbType.Oracle,//设置数据库类型
+                new ConnectionConfig()
+                {
+                    ConnectionString = System.Web.Configuration.WebConfigurationManager.AppSettings["ConnectionString"],
+                    DbType = DbType.Oracle,//设置数据库类型
                     IsAutoCloseConnection = true,//自动释放数据务，如果存在事务，在事务结束后释放
                     InitKeyType = InitKeyType.Attribute //从实体特性中读取主键自增列信息
                 });
@@ -142,13 +137,13 @@ namespace Adventure.Controllers
                 DateTime dt_start_time, dt_end_time;
                 DateTimeFormatInfo dtFormat = new DateTimeFormatInfo();
                 dtFormat.ShortDatePattern = "yyyy/MM/dd";
-                dt_start_time = Convert.ToDateTime(form["form-start-time"], dtFormat);
-                dt_end_time = Convert.ToDateTime(form["form-end-time"], dtFormat);
-                string key = form["form-address"].ToLower();
-                int numNeed = Convert.ToInt32(form["form-member-num"]);
+                dt_start_time = Convert.ToDateTime(form["start_time"], dtFormat);
+                dt_end_time = Convert.ToDateTime(form["end_time"], dtFormat);
+                string key = form["destination"].ToLower();
+                int numNeed = Convert.ToInt32(form["person_num"]);
 
                 var availableID = new List<int>();
-                var available = db.Queryable<ActivityInstance>().Where(it => (DateTime.Compare(dt_start_time, it.end_time) >= 0 || DateTime.Compare(dt_end_time, it.start_time) <= 0) && it.is_booked == 0).ToArray();
+                var available = db.Queryable<ActivityInstance>().Where(it => (dt_start_time >= it.end_time || dt_end_time <= it.start_time) && it.is_booked == 0).ToArray();
                 for (int i = 0; i < available.Length; i++)
                 {
                     if (availableID.Contains(available[i].activity_id) == false)
